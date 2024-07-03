139cc66cae9e418c8ae0385fc74cdf3a9432671f
wangyefeng <wangyefeng@tuyoogame.com>
2024-07-03 21:58:36 +0800

    优化网络层参数

--- src/main/java/org/wyf/server/EchoServer.java
+++ src/main/java/org/wyf/server/EchoServer.java
@@ -1,10 +1,10 @@
 package org.wyf.server;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.EventLoopGroup;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.*;
+import io.netty.channel.epoll.Epoll;
+import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -27,18 +27,26 @@ public class EchoServer {
         try {
             ServerBootstrap bootstrap = new ServerBootstrap();
             bootstrap.group(bossGroup, workerGroup)
-                     .channel(NioServerSocketChannel.class)
+                     .channel(Epoll.isAvailable() ? EpollServerSocketChannel.class : NioServerSocketChannel.class)
                      .childHandler(new ChannelInitializer<SocketChannel>() {
                          @Override
-                         public void initChannel(SocketChannel ch) throws Exception {
+                         public void initChannel(SocketChannel ch) {
                              ChannelPipeline pipeline = ch.pipeline();
-                             pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
+                             pipeline.addLast(new LengthFieldBasedFrameDecoder(1024 * 10, 0, 4, 0, 4));
                              pipeline.addLast(new ProtobufDecoder());
                              pipeline.addLast(new ProtobufEncode());
                              pipeline.addLast(new EchoServerHandler());
                          }
                      });
+            bootstrap.option(ChannelOption.SO_BACKLOG, 200);
+            bootstrap.option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
+            bootstrap.option(ChannelOption.SO_REUSEADDR, true); // 允许端口复用
 
+            bootstrap.childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
+            bootstrap.childOption(ChannelOption.SO_KEEPALIVE, false);// 禁用tcp保活机制，自定义ping包检测tcp链接是否正常
+            bootstrap.childOption(ChannelOption.TCP_NODELAY, true);// 禁用Nagle算法
+            bootstrap.childOption(ChannelOption.SO_RCVBUF, 1024 * 128); // 设置接收缓冲区大小
+            bootstrap.childOption(ChannelOption.SO_SNDBUF, 1024 * 128); // 设置发送缓冲区大小
             // 绑定端口并启动服务器
             ChannelFuture future = bootstrap.bind(port).sync();
             System.out.println("EchoServer started and listening on port " + port);
